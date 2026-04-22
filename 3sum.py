9c41edc6d2ce91a6f3e76d759ce09f7cf6ad3533
Aniket Kumar <aniket.kumardeoghar2001@gmail.com>
2026-04-22 23:52:06 +0530

    Updated

--- 3sum.py
+++ 3sum.py
@@ -22,3 +22,12 @@ class Solution:
 
                 if total == 0:
                     result.append([nums[i], nums[left], nums[right]])
+
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    
+                    left += 1
+                    right -= 1
