cbe1f9fd5aab033e925d4ba260fa2d3e4ebdc11c
Aniket Kumar <aniket.kumardeoghar2001@gmail.com>
2026-04-25 02:18:32 +0530

    solved leet code question

---
+++ cointainer-with-most-water.py
@@ -0,0 +1,22 @@
+class Solution:
+    from typing import List
+    def maxArea(self, height: List[int]) -> int:
+
+        left = 0
+        right = len(height) - 1
+        max_water = 0
+
+        while left < right:
+
+            width = right - left
+            h = min(height[left], height[right])
+            current_water = width * h
+
+            max_water = max(max_water, current_water)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_water
