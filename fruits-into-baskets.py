e205fda01f4053629ee39821fcb55621d6b96d7b
Aniket Kumar <aniket.kumardeoghar2001@gmail.com>
2026-05-02 23:57:00 +0530

    updated

--- fruits-into-baskets.py
+++ fruits-into-baskets.py
@@ -4,3 +4,9 @@ class Solution:
         basket = {}      # fruit_type → count in window
         left = 0
         max_fruits = 0
+
+        for right in range(len(fruits)):
+
+            # Step 1: Add right fruit to basket
+            fruit = fruits[right]
+            basket[fruit] = basket.get(fruit, 0) + 1
