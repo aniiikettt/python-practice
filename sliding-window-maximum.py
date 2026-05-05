4cb1b1f28b49ef4d8746f864018fa8e38ac5e55c
Aniket Kumar <aniket.kumardeoghar2001@gmail.com>
2026-05-05 17:35:30 +0530

    updated code

--- sliding-window-maximum.py
+++ sliding-window-maximum.py
@@ -5,3 +5,10 @@ class Solution:
         
         dq = deque()    # Stores INDICES, front = index of max
         result = []
+
+        for right in range(len(nums)):
+
+            # Step 1: Remove EXPIRED index from front
+            # (index no longer in window [right-k+1 ... right])
+            if dq and dq[0] < right - k + 1:
+                dq.popleft()
