80e871653c1e6eab67f033e58ca5c365f9830265
Aniket Kumar <aniket.kumardeoghar2001@gmail.com>
2026-05-10 21:49:02 +0530

    updated the code

--- matrix-block-sum.py
+++ matrix-block-sum.py
@@ -4,3 +4,10 @@ class Solution:
         m, n = len(mat), len(mat[0])
 
         prefix = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                prefix[i][j] = (mat[i-1][j-1]
+                              + prefix[i-1][j]    # top
+                              + prefix[i][j-1]    # left
+                              - prefix[i-1][j-1]) # remove overlap
