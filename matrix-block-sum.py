a4c243bdaeca0a71f7f2597eed73d3a55473477e
Aniket Kumar <aniket.kumardeoghar2001@gmail.com>
2026-05-10 21:48:18 +0530

    updated the code

--- matrix-block-sum.py
+++ matrix-block-sum.py
@@ -2,3 +2,5 @@ class Solution:
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
         
         m, n = len(mat), len(mat[0])
+
+        prefix = [[0] * (n + 1) for _ in range(m + 1)]
